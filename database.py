5cb089e03e597507931caa4974b7cc4da9e0ed0a
xdlmaopff <118069347+xdlmaopff@users.noreply.github.com>
2025-12-14 18:22:30 +0300

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import sqlite3
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./botnet.db")
 if DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(DATABASE_URL, connect_args=connect_args, echo=False)
 
